5e1b67ab6ca0c78124f53721a693541d4a1f8330
Milos Bobic <loschmie@gmail.com>
2020-02-20 12:31:11 +0100

    travis support added

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 test('renders sipu_racku link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/sipu_racku.ne.com/i);
